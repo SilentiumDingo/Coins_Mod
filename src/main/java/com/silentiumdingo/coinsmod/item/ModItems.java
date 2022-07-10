46a41bb06de237d5a028b34d32bfb2c2dca43f39
SilentiumDingo <SilentiumDingo@gmail.com>
2022-07-09 23:57:10 -0400

    Added textures and gold/silver coins

--- src/main/java/com/silentiumdingo/coinsmod/item/ModItems.java
+++ src/main/java/com/silentiumdingo/coinsmod/item/ModItems.java
@@ -12,9 +12,14 @@ public class ModItems {
     public static final DeferredRegister<Item> ITEMS =
             DeferredRegister.create(ForgeRegistries.ITEMS, CoinsMod.MOD_ID);
 
-    public static final RegistryObject<Item> CopperCoin = ITEMS.register("copper_coin",
+    public static final RegistryObject<Item> COPPERCOIN = ITEMS.register("coppercoin",
             () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
 
+    public static final RegistryObject<Item> SILVERCOIN = ITEMS.register("silvercoin",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+
+    public static final RegistryObject<Item> GOLDCOIN = ITEMS.register("goldcoin",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
