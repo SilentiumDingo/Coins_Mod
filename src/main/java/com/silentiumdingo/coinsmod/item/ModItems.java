99a76e47d07a5b9c52e99b6b783c5991c9253b1e
SilentiumDingo <SilentiumDingo@gmail.com>
2022-07-09 22:29:53 -0400

    Added Items

---
+++ src/main/java/com/silentiumdingo/coinsmod/item/ModItems.java
@@ -0,0 +1,22 @@
+package com.silentiumdingo.coinsmod.item;
+
+import com.silentiumdingo.coinsmod.CoinsMod;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, CoinsMod.MOD_ID);
+
+    public static final RegistryObject<Item> CopperCoin = ITEMS.register("copper_coin",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+}
